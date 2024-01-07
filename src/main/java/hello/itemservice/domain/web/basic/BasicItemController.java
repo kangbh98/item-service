a87fa9d18e8348572cda9178707db8dab51a12a8
BH kang <rkdqudgjs1127@gmail.com>
2024-01-08 01:55:09 +0900

    ㅂㅁ

--- src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/domain/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -34,14 +35,15 @@ public class BasicItemController {
 
     @GetMapping("/add")
     public String addForm(){
-        return "/basic/addForm";
+        return "basic/addForm";
     }
     @PostMapping("/add")
     public String save(
             @RequestParam("gasName") String gasName,
             @RequestParam("harmful") int harmful,
             @RequestParam("quantity") int quantity,
-            Model model
+            Model model,
+            RedirectAttributes redirectAttributes
     ){
         Gas gas = new Gas();
         gas.setGasName(gasName);
@@ -50,14 +52,17 @@ public class BasicItemController {
         gasRepository.save(gas);
 
         model.addAttribute("gas", gas);
-        return "/basic/item";
+
+        redirectAttributes.addAttribute("gasId", gas.getId());
+        redirectAttributes.addAttribute("status",true);
+        return "redirect:/basic/items/{gasId}";
     }
 
     @GetMapping("/{gasId}/edit")
     public String editForm(@PathVariable("gasId") Long gasId, Model model) {
         Gas byId = gasRepository.findById(gasId);
         model.addAttribute("gas", byId);
-        return "/basic/editForm";
+        return "basic/editForm";
     }
 
     @PostMapping("/{gasId}/edit")
